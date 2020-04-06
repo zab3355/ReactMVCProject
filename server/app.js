b54b9a233417546a8007cf695f4a7c4e6a64bf8d
Zach Brown <zab5957@rit.edu>
2020-04-06 13:22:04 -0400

    DomoMaker D Updates

---
+++ server/app.js
@@ -0,0 +1,97 @@
+// Import libraries
+const path = require('path');
+const express = require('express');
+const compression = require('compression');
+const favicon = require('serve-favicon');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const expressHandlebars = require('express-handlebars');
+const session = require('express-session');
+const RedisStore = require('connect-redis')(session);
+const url = require('url');
+const csrf = require('csurf');
+const redis = require('redis');
+
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+const dbURL = process.env.MONGODB_URI || 'mongodb://localhost/DomoMaker';
+
+// Setup mongoose options to use newer functionality
+const mongooseOptions = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+mongoose.connect(dbURL, mongooseOptions, (err) => {
+  if (err) {
+    console.log('Could not connect to database');
+    throw err;
+  }
+});
+
+let redisURL = {
+    //You will need to follow the "Setting up Redis for Local Use" Instructions
+    hostname: 'redis-11966.c12.us-east-1-4.ec2.cloud.redislabs.com',
+    //replace this with the port number of your endpoint url
+    port: '11966',
+};
+
+let redisPASS = 'qxnYKBGgRAYOmsugNl301mG1mDupOUYK';
+if (process.env.REDISCLOUD_URL){
+    redisURL = url.parse(process.env.REDISCLOUD_URL);
+    [, redisPASS] = redisURL.auth.split(':');
+}
+let redisClient = redis.createClient({
+    host: redisURL.hostname, 
+    port: redisURL.port,
+    password: redisPASS,
+});
+
+// Pull in our routes
+const router = require('./router.js');
+
+const app = express();
+app.use('/assets', express.static(path.resolve(`${__dirname}/../hosted/`)));
+app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
+app.use(compression());
+app.use(bodyParser.urlencoded({
+  extended: true,
+}));
+
+app.use(session({
+    key: 'sessionid',
+    store: new RedisStore({
+        client: redisClient,
+    }),
+    secret: 'Domo Arigato',
+    resave: true,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+    },
+}));
+
+app.engine('handlebars', expressHandlebars({ defaultLayout: 'main', }));
+app.set('view engine', 'handlebars');
+app.set('views', `${__dirname}/../views`);
+app.disable('x-powered-by');
+app.use(cookieParser());
+
+app.use(csrf());
+app.use((err, req, res, next) => {
+    if(err.code !== 'EBADCSRFTOKEN') return next(err);
+    
+    console.log('Missing CSRF token');
+    return false;
+});
+
+router(app);
+
+app.listen(port, (err) => {
+  if (err) {
+    throw err;
+  }
+
+  console.log(`Listening on port ${port}`);
+});
