8914963f1c88896051bace5ce332dc01f92153c3
Zach Brown <zab5957@rit.edu>
2020-04-07 18:03:14 -0400

    Completed DomoMaker D

---
+++ hosted/bundle.js
@@ -0,0 +1,142 @@
+"use strict";
+
+var handleDomo = function handleDomo(e) {
+  e.preventDefault();
+  $("#domoMessage").animate({
+    width: 'hide'
+  }, 350);
+
+  if ($("#domoName").val() == '' || $("#domoAge").val() == '') {
+    handleError("RAWR! All fields are required");
+    return false;
+  }
+
+  sendAjax('POST', $("#domoForm").attr("action"), $("#domoForm").serialize(), function () {
+    //get csrf token to send to new Domo
+    var csrf = document.querySelector('#domoForm').querySelector('#csrfToken').value;
+    loadDomosFromServer(csrf);
+  });
+  return false;
+};
+
+var DomoForm = function DomoForm(props) {
+  return /*#__PURE__*/React.createElement("form", {
+    id: "domoForm",
+    name: "domoForm",
+    onSubmit: handleDomo,
+    action: "/maker",
+    method: "POST",
+    className: "domoForm"
+  }, /*#__PURE__*/React.createElement("label", {
+    htmlFor: "name"
+  }, "Name: "), /*#__PURE__*/React.createElement("input", {
+    id: "domoName",
+    type: "text",
+    name: "name",
+    placeholder: "Domo Name"
+  }), /*#__PURE__*/React.createElement("label", {
+    htmlFor: "age"
+  }, "Age: "), /*#__PURE__*/React.createElement("input", {
+    id: "domoAge",
+    type: "text",
+    name: "age",
+    placeholder: "Domo Age"
+  }), /*#__PURE__*/React.createElement("input", {
+    id: "csrfToken",
+    type: "hidden",
+    name: "_csrf",
+    value: props.csrf
+  }), /*#__PURE__*/React.createElement("input", {
+    className: "makeDomoSubmit",
+    type: "submit",
+    value: "Make Domo"
+  }));
+};
+
+var DomoList = function DomoList(props) {
+  if (props.domos.length === 0) {
+    return /*#__PURE__*/React.createElement("div", {
+      className: "domoList"
+    }, /*#__PURE__*/React.createElement("h3", {
+      className: "emptyDomo"
+    }, "No Domos Yet"));
+  }
+
+  var domoNodes = props.domos.map(function (domo) {
+    return /*#__PURE__*/React.createElement("div", {
+      key: domo._id,
+      className: "domo"
+    }, /*#__PURE__*/React.createElement("img", {
+      src: "/assets/img/domoface.jpeg",
+      alt: "domo face",
+      className: "domoFace"
+    }), /*#__PURE__*/React.createElement("h3", {
+      className: "domoName"
+    }, "Name: ", domo.name), /*#__PURE__*/React.createElement("h3", {
+      className: "domoAge"
+    }, "Age: ", domo.age));
+  });
+  return /*#__PURE__*/React.createElement("div", {
+    className: "domoList"
+  }, domoNodes);
+};
+
+var loadDomosFromServer = function loadDomosFromServer(csrf) {
+  sendAjax('GET', '/getDomos', null, function (data) {
+    ReactDOM.render( /*#__PURE__*/React.createElement(DomoList, {
+      csrf: csrf,
+      domos: data.domos
+    }), document.querySelector("#domos"));
+  });
+};
+
+var setup = function setup(csrf) {
+  ReactDOM.render( /*#__PURE__*/React.createElement(DomoForm, {
+    csrf: csrf
+  }), document.querySelector("#makeDomo"));
+  ReactDOM.render( /*#__PURE__*/React.createElement(DomoList, {
+    csrf: csrf,
+    domos: []
+  }), document.querySelector("#domos"));
+  loadDomosFromServer(csrf);
+};
+
+var getToken = function getToken() {
+  sendAjax('GET', '/getToken', null, function (result) {
+    setup(result.csrfToken);
+  });
+};
+
+$(document).ready(function () {
+  getToken();
+});
+"use strict";
+
+var handleError = function handleError(message) {
+  $("#errorMessage").text(message);
+  $("#domoMessage").animate({
+    width: 'toggle'
+  }, 350);
+};
+
+var redirect = function redirect(response) {
+  $("#domoMessage").animate({
+    width: 'hide'
+  }, 350);
+  window.location = response.redirect;
+};
+
+var sendAjax = function sendAjax(type, action, data, success) {
+  $.ajax({
+    cache: false,
+    type: type,
+    url: action,
+    data: data,
+    dataType: "json",
+    success: success,
+    error: function error(xhr, status, _error) {
+      var messageObj = JSON.parse(xhr.responseText);
+      handleError(messageObj.error);
+    }
+  });
+};
