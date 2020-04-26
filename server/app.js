56c5de06f93fbacbcfac089ec68a6a0c1884be15
Zach Brown <zab5957@rit.edu>
2020-04-26 03:17:40 -0400

    app changes

--- server/app.js
+++ server/app.js
@@ -1,4 +1,10 @@
-// Import libraries
+
+/* eslint-disable linebreak-style */
+/* eslint-disable prefer-destructuring */
+/* eslint-disable no-console */
+/* eslint-disable linebreak-style */
+
+// import libraries
 const path = require('path');
 const express = require('express');
 const compression = require('compression');
@@ -7,83 +13,72 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const expressHandlebars = require('express-handlebars');
+const redis = require('redis');
+
+// Connecting redis package
 const session = require('express-session');
 const RedisStore = require('connect-redis')(session);
+
 const url = require('url');
 const csrf = require('csurf');
-const redis = require('redis');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 const dbURL = process.env.MONGODB_URI || 'mongodb://localhost/RecipeMaker';
 
-// Setup mongoose options to use newer functionality
-const mongooseOptions = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-};
-
-mongoose.connect(dbURL, mongooseOptions, (err) => {
+/* Connecting mongoose */
+mongoose.connect(dbURL, (err) => {
   if (err) {
-    console.log('Could not connect to database');
+    console.log('Could not connect to the database');
     throw err;
   }
 });
 
-let redisURL = {
-    //You will need to follow the "Setting up Redis for Local Use" Instructions
+/* Connecting redis */
+const client = redis.createClient({
     hostname: 'redis-11966.c12.us-east-1-4.ec2.cloud.redislabs.com',
     //replace this with the port number of your endpoint url
     port: '11966',
-};
-
-let redisPASS = 'qxnYKBGgRAYOmsugNl301mG1mDupOUYK';
-if (process.env.REDISCLOUD_URL){
-    redisURL = url.parse(process.env.REDISCLOUD_URL);
-    [, redisPASS] = redisURL.auth.split(':');
-}
-let redisClient = redis.createClient({
-    host: redisURL.hostname, 
-    port: redisURL.port,
-    password: redisPASS,
+  password: 'qxnYKBGgRAYOmsugNl301mG1mDupOUYK',
 });
 
-// Pull in our routes
+if (process.env.REDISCLOUD_URL) {
+  client.host = url.parse(process.env.REDISCLOUD_URL);
+  client.password = client.host.auth.split(':')[1];
+}
+
+// pull in our routes
 const router = require('./router.js');
 
+/* Using express */
 const app = express();
 app.use('/assets', express.static(path.resolve(`${__dirname}/../hosted/`)));
-app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
+app.use(favicon(`${__dirname}/../hosted/img/peanutFavicon.png`));
+app.disable('x-powered-by');
 app.use(compression());
 app.use(bodyParser.urlencoded({
   extended: true,
 }));
-
 app.use(session({
-    key: 'sessionid',
-    store: new RedisStore({
-        client: redisClient,
-    }),
-    secret: 'Recipe Arigato',
-    resave: true,
-    saveUninitialized: true,
-    cookie: {
-        httpOnly: true,
-    },
+  key: 'sessionid',
+  store: new RedisStore({ client }),
+  secret: 'Chicken Nuggets',
+  resave: 'true',
+  saveUninitialized: true,
+  // prevents client side JS from accessing cookies
+  cookie: {
+    httpOnly: true,
+  },
 }));
-
-app.engine('handlebars', expressHandlebars({ defaultLayout: 'main', }));
+app.engine('handlebars', expressHandlebars({ defaultLayout: '' }));
 app.set('view engine', 'handlebars');
 app.set('views', `${__dirname}/../views`);
-app.disable('x-powered-by');
 app.use(cookieParser());
-
 app.use(csrf());
 app.use((err, req, res, next) => {
-    if(err.code !== 'EBADCSRFTOKEN') return next(err);
-    
-    console.log('Missing CSRF token');
-    return false;
+  if (err.code !== 'EBADCSRFTOKEN') return next(err);
+  console.log('Missing CSRF token');
+  return false;
 });
 
 router(app);
@@ -92,6 +87,5 @@ app.listen(port, (err) => {
   if (err) {
     throw err;
   }
-
   console.log(`Listening on port ${port}`);
 });
