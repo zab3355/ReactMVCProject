375af6fb6920ccceffbeaa316aff16b327ced2ac
Zach Brown <zab5957@rit.edu>
2020-04-26 19:27:57 -0400

    client updates

--- client/app/maker.js
+++ client/app/maker.js
@@ -23,134 +23,168 @@ const handleRecipe = (e) =>{
   return false;
 };
 
+var handleRecipe = function handleRecipe(e) {
+  e.preventDefault();
+  $("#errormessage").animate({
+    width: 'hide'
+  }, 350);
 
-
-const RecipeForm = (props) =>{
-  return(
-  <form id="recipeForm" name="recipeForm"
-        onSubmit={handleRecipe}
-        action="/addRecipe"
-        method='POST'
-        className="recipeForm"
-    >
-    <input id="recipeName" type="text" name="name" placeholder="Recipe Name" />    
-    <select className="select-option" id="foodCategory" name="category">
-            <option selected="selected" disabled="disabled" value="start">Category: </option>
-            <option value="Chinese">Chinese</option>
-            <option value="American">American</option>
-            <option value="Mexican">Mexican</option>
-            <option value="Indian">Indian</option>
-            <option value="Indian">Indian</option>
-            <option value="Japanese">Japanese</option>
-            <option value="Italian">Italian</option>
-      </select>
-      
-    <select className="select-option" id="priceCateogry" name="price">
-            <option selected="selected" disabled="disabled" value="start">Price: </option>
-            <option value="$">$</option>
-            <option value="$$">$$</option>
-            <option value="$$$">$$$</option>
-            <option value="$$$$">$$$$</option>
-      </select>
-      
-    <select className="select-option" id="tasteCategory" name="taste">
-            <option selected="selected" disabled="disabled" value="start">Price: </option>
-            <option value="Sour">Sour</option>
-            <option value="Sweet">Sweet</option>
-            <option value="Spicy">Spicy</option>
-            <option value="Bitter">Bitter</option>
-      </select>
-    <input id="csrfToken" type="hidden" name="_csrf" value={props.csrf} />
-    <input className="makeRecipeSubmit" type="submit" value="Add Recipe" />
-  </form>
-  );
-};
-
-const RecipeList = function(props){
-  if(props.recipes.length === 0){
-    return(
-      <div className="recipeList">
-        <h3 className="emptyRecipe">No Recipes Listed!</h3>
-      </div>
-    );
-  };
-  
-  const recipeNodes = props.recipes.map(function(recipe){
-    return(
-  <form id="recipeForm" name="recipeForm"
-        onSubmit={handleRecipe}
-        action="/information"
-        method='POST'
-        className="recipeForm"
-    >
-      <div key={recipe._id} className="recipe">
-       <div className="recipeNode" id="recipeNodeShow" onclick="showRecipe()">
-        <img src="/assets/img/cartIcon.png" alt="recipe cart icon" className="cartIcon" />
-        <h3 className="recipeName">Recipe Name: {recipe.name}</h3> 
-        <p className="foodCategory">Category: {recipe.category}</p>
-        <p className="tasteCategory">Taste: {recipe.taste}</p>
-        <p className="priceCategory">Price: {recipe.price}</p>
-        <input className="makeRecipeSubmit" type="submit" value="Information" />
-      </div>
-    </div>
-    </form>
-    );
-  });
-  
-  return(
-    <div className="recipeList">
-      {recipeNodes}
-    </div>
-  );
-};
-
-// Handling Password Change Form
-const handleChangePass = (e) => {
-    e.preventDefault();
-    $('#error').fadeOut(200);
-  
-    if ($('#oldPass').val() == '' || $('#newPass').val() == '' || $('#newPass2').val() == '') {
-      handleError('All fields are required');
-      return false;
-    }
-  
-    if ($('#newPass').val() !== $('#newPass2').val()) {
-      handleError('Passwords do not match');
-      return false;
-    }
-  
-    $('#error').fadeIn(200);
-    /* Otherwise continue loading new page */
-    makeAjaxCallback($('#changePassword').attr('action'), $('#changePassword').serialize(), (data) => {
-      handleSuccess('Password changed');
-    $("#success").animate({width: 'hide'}, 350);
-    });
-  
+  if ($("#recipeName").val() == '' || $("#foodCategory").val() == '' || $("#priceCategory").val() == '' || $("#tasteCategory").val() == '') {
+    handleError("All fields are required!");
     return false;
-  };
+  }
 
+  sendAjaxCall('POST', $("#recipeForm").attr("action"), $("#recipeForm").serialize(), function () {
+    //get csrf token to send to new Recipe
+    var csrf = document.querySelector('#recipeForm').querySelector('#csrfToken').value;
+    loadRecipesFromServer(csrf);
+  });
+    
+  $('#tasteCategory').value = '';
+  $('#priceCategory').value = '';
+  $('#foodCategory').value = 'start';
+    
+  return false;
+};
 
-//Password Change Form
-  const ChangePassForm = (props) => {
-    // webkit text security from https://stackoverflow.com/questions/1648665/changing-the-symbols-shown-in-a-html-password-field -->
-    return (
-      <form id="changePassword" name="changePassword" 
-      action="/changePassword" method="POST" 
-      class="recipeForm" onSubmit={handleChangePass}>
-        <input id="oldPass" type="text" name="oldPass" placeholder="Old Password" />
-        <input id="newPass" type="text" name="newPass" placeholder="New Password" />
-        <input id="newPass2" type="text" name="newPass2" placeholder="Repeat New Password" />
-        <input type="hidden" name="_csrf" value={props.csrf} />
-        <input className="formSubmit" type="submit" value="Change Password" />
-    </form>
-    )
-  };
+var RecipeForm = function RecipeForm(props) {
+  return /*#__PURE__*/React.createElement("form", {
+    id: "recipeForm",
+    name: "recipeForm",
+    onSubmit: handleRecipe,
+    action: "/addRecipe",
+    method: "POST",
+    className: "recipeForm"
+  },/*#__PURE__*/React.createElement("input", {
+    id: "recipeName",
+    type: "text",
+    name: "name",
+    placeholder: "Recipe Name"
+  }), 
+/*#__PURE__*/React.createElement("select", {
+    id: "foodCategory",
+    name: "category",
+    className: "select-option",
+  },
+/*#__PURE__*/React.createElement("option", {
+    selected: "selected",
+    disabled: "disabled",
+    value: "start"
+  }, "Category:"),
+/*#__PURE__*/React.createElement("option", {
+    value: "Chinese Cuisine"
+  }, "Chinese Cuisine"),
+/*#__PURE__*/React.createElement("option", {
+    value: "American Cuisine"
+  }, "American Cuisine"), 
+/*#__PURE__*/React.createElement("option", {
+    value: "Mexican Cuisine"
+  }, "Mexican Cuisine"), 
+/*#__PURE__*/React.createElement("option", {
+    value: "Indian Cuisine"
+  }, "Indian Cuisine"),
+/*#__PURE__*/React.createElement("option", {
+    value: "Japanese Cuisine"
+  }, "Japanese Cuisine"), 
+/*#__PURE__*/React.createElement("option", {
+    value: "Italian Cuisine"
+  }, "Italian Cuisine")), 
 
-//Password Change Form Setup
-const setupPassChangeForm = function(csrf) {
-    ReactDOM.render(
-        <ChangePassForm csrf={csrf} />, document.querySelector("#changePassForm")
-    );
+/*#__PURE__*/React.createElement("select", {
+    id: "priceCategory",
+    name: "price",
+    className: "select-option",
+  },
+/*#__PURE__*/React.createElement("option", {
+    selected: "selected",
+    disabled: "disabled",
+    value: "start"
+  }, "Price:"),
+/*#__PURE__*/React.createElement("option", {
+    value: "$"
+  }, "$"),
+/*#__PURE__*/React.createElement("option", {
+    value: "$$"
+  }, "$$"), 
+/*#__PURE__*/React.createElement("option", {
+    value: "$$$"
+  }, "$$$"), 
+/*#__PURE__*/React.createElement("option", {
+    value: "$$$$"
+  }, "$$$$")),                                        
+/*#__PURE__*/React.createElement("select", {
+    id: "tasteCategory",
+    name: "taste",
+    className: "select-option",
+  },
+/*#__PURE__*/React.createElement("option", {
+    selected: "selected",
+    disabled: "disabled",
+    value: "start"
+  }, "Taste:"),
+/*#__PURE__*/React.createElement("option", {
+    value: "Sour"
+  }, "Sour"),
+/*#__PURE__*/React.createElement("option", {
+    value: "Sweet"
+  }, "Sweet"), 
+/*#__PURE__*/React.createElement("option", {
+    value: "Spicy"
+  }, "Spicy"), 
+/*#__PURE__*/React.createElement("option", {
+    value: "Bitter"
+  }, "Bitter")),                                               
+/*#__PURE__*/React.createElement("input", {
+    id: "csrfToken",
+    type: "hidden",
+    name: "_csrf",
+    value: props.csrf
+  }), /*#__PURE__*/React.createElement("input", {
+    className: "makeRecipeSubmit",
+    type: "submit",
+    value: "Add Recipe"
+  }));
+};
+
+var RecipeList = function RecipeList(props) {
+  if (props.recipes.length === 0) {
+    return /*#__PURE__*/React.createElement("div", {
+      className: "recipeList"
+    }, /*#__PURE__*/React.createElement("h3", {
+      className: "emptyRecipe"
+    }, "No Recipes Listed!"));
+  }
+
+  var recipeNodes = props.recipes.map(function (recipe) {
+    return /*#__PURE__*/React.createElement("div", {
+      key: recipe._id,
+      className: "recipe"
+    }, /*#__PURE__*/React.createElement("img", {
+      src: "/assets/img/cartIcon.png",
+      alt: "recipe cart icon",
+      className: "cartIcon"
+    }), /*#__PURE__*/React.createElement("h3", {
+      className: "recipeName"
+    }, "Recipe Name: ", recipe.name),
+/*#__PURE__*/React.createElement("p", {
+      className: "foodCategory"
+    }, "Category: ", recipe.category),
+/*#__PURE__*/React.createElement("p", {
+      className: "tasteCategory"
+    }, "Taste: ", recipe.taste),
+/*#__PURE__*/React.createElement("p", {
+      className: "priceCategory"
+    }, "Price: ", recipe.price),
+/*#__PURE__*/React.createElement("input", {
+    className: "makeRecipeSubmit",
+    type: "submit",
+    id: "information",
+    value: "Information"
+  }));
+  });
+  return /*#__PURE__*/React.createElement("div", {
+    className: "recipeList"
+  }, recipeNodes);
 };
 
 const loadRecipesFromServer = (csrf) =>{
---
+++ client/settings/settings.js
@@ -0,0 +1,51 @@
+// Handling Password Change Form
+const handleChangePass = (e) => {
+    e.preventDefault();
+  
+  $("#errorMessage").animate({width: 'hide'}, 350);
+  $("#success").animate({width: 'hide'}, 350);
+  
+  
+    if ($('#oldPass').val() == '' || $('#newPass').val() == '' || $('#newPass2').val() == '') {
+      handleError('All fields are required');
+      return false;
+    }
+  
+    if ($('#newPass').val() !== $('#newPass2').val()) {
+      handleError('Passwords do not match');
+      return false;
+    }
+  
+    $('#error').fadeIn(200);
+    /* Otherwise continue loading new page */
+    makeAjaxCallback($('#changePassword').attr('action'), $('#changePassword').serialize(), (data) => {
+      handleSuccess('Password changed');
+    $("#success").animate({width: 'hide'}, 350);
+    });
+  
+    return false;
+  };
+
+
+//Password Change Form
+  const ChangePassForm = (props) => {
+    // webkit text security from https://stackoverflow.com/questions/1648665/changing-the-symbols-shown-in-a-html-password-field -->
+    return (
+      <form id="changePassword" name="changePassword" 
+      action="/changePassword" method="POST" 
+      class="recipeForm" onSubmit={handleChangePass}>
+        <input id="oldPass" type="text" name="oldPass" placeholder="Old Password" />
+        <input id="newPass" type="text" name="newPass" placeholder="New Password" />
+        <input id="newPass2" type="text" name="newPass2" placeholder="Repeat New Password" />
+        <input type="hidden" name="_csrf" value={props.csrf} />
+        <input className="formSubmit" type="submit" value="Change Password" />
+    </form>
+    )
+  };
+
+//Password Change Form Setup
+const setupPassChangeForm = function(csrf) {
+    ReactDOM.render(
+        <ChangePassForm csrf={csrf} />, document.querySelector("#changePassForm")
+    );
+};
