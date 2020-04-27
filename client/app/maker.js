a793d9f7c547a7cb3072d9125ae0a8470d303df1
Zach Brown <zab5957@rit.edu>
2020-04-27 18:35:52 -0400

    Adjusted client

--- client/app/maker.js
+++ client/app/maker.js
@@ -12,6 +12,11 @@ const handleRecipe = (e) =>{
     return false;
   }
     
+  if($("#searchByTaste").val() ==''){
+    handleError('Please fill out search!');
+      return false;
+  }
+  
   //Ajax call to send Recipe Form
   sendAjaxCall('POST', $("#recipeForm").attr("action"), $("#recipeForm").serialize(), function(){
     
@@ -103,6 +108,59 @@ const recipeNodes = props.recipes.map(function (recipe){
   );
 };
 
+// Handling Password Change Form
+const handleChangePass = (e) => {
+    e.preventDefault();
+  $("#errorMessage").animate({width: 'hide'}, 350);
+  
+    if ($('#oldPass').val() == '' || $('#newPass').val() == '' || $('#newPass2').val() == '') {
+      handleError('Please fill out each field!');
+      return false;
+    }
+  
+    if ($('#newPass').val() !== $('#newPass2').val()) {
+      handleError('Passwords do not match');
+      return false;
+    }
+  
+  $("#errorMessage").animate({
+    width: 'toggle'
+  }, 350);
+    
+    //Load the success message and change password if there are no errors
+    makeAjaxCallback($('#changePassword').attr('action'), $('#changePassword').serialize(), (data) => {
+    //success handler
+      handleSuccess('Password changed!');
+    $("#success").text="Success!";
+    $("#success").animate({width: 'hide'}, 350);
+    });
+  
+    return false;
+  };
+
+
+//Password Change Form
+  const ChangePassForm = (props) => {
+    return (
+      <form id="changePassword" name="changePassword" 
+      action="/changePassword" method="POST" 
+      class="recipeForm" onSubmit={handleChangePass}>
+        <input id="oldPass" type="password" name="oldPass" placeholder="Old Password" />
+        <input id="newPass" type="password" name="newPass" placeholder="New Password" />
+        <input id="newPass2" type="password" name="newPass2" placeholder="Repeat New Password" />
+        <input type="hidden" name="_csrf" value={props.csrf} />
+        <input className="formSubmit" type="submit" value="Change Password" />
+    </form>
+    )
+  };
+
+//Password Change Form Setup
+const setupPassChangeForm = function(csrf) {
+    ReactDOM.render(
+        <ChangePassForm csrf={csrf} />, document.querySelector("#changePassForm")
+    );
+};
+
 //load getRecipeItems
 const loadRecipesFromServer = (csrf) =>{
   sendAjaxCall('GET', '/getRecipeItems', null, (data) =>{
@@ -130,9 +188,14 @@ const setup = function(csrf){
 
 
 //based upon URL, will setup specific renders
-const getToken = () =>{
+const getToken = (url) =>{
   sendAjaxCall('GET', '/getToken', null, (result) =>{
+      if(window.location.href.indexOf("addRecipe") > -1){
         setup(result.csrfToken);
+    }
+    if(window.location.href.indexOf("account") > -1) {
+        setupPassChangeForm(result.csrfToken);
+        } 
     });
 };
 
